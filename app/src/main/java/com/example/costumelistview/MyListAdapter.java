1769bf6b0f466e4714c268458c93236f2a337a80
indragunawan222 <ig225970@gmail.com>
2019-08-05 21:20:59 +0700

    Custom List

---
+++ app/src/main/java/com/example/costumelistview/MyListAdapter.java
@@ -0,0 +1,11 @@
+package com.example.costumelistview;
+
+import com.example.danianwar.my_custom_list.Hero;
+import com.example.danianwar.my_custom_list.MainActivity;
+
+import java.util.List;
+
+public class MyListAdapter {
+    public MyListAdapter(MainActivity mainActivity, int my_custom_list, List<Hero> heroList) {
+    }
+}
